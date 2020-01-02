abf8c912ecc37ca20f9642ebe1ec0648ef01f3c7
Zack Yancey <mail@zackyancey.com>
2020-01-02 10:59:45 -0700

    Add a basic prompt for where sbp is slow

---
+++ .dotfiles/basic_prompt.bash
@@ -0,0 +1,11 @@
+# In some environments calls to git or sbp can be really slow (ie Cygwin on
+# Windows with corporate antivirus...). In those environments this prompt
+# covers the basics.
+
+function nonzero_return() {
+  RETVAL=$?
+  [ $RETVAL -ne 0 ] && echo "$RETVAL "
+}
+
+export PS1='\n\[\e[32m\]\u@\h\[\e[0m\] \[\e[36m\]\w\[\e[0m\]\n \[\e[91m\]`nonzero_return`\[\e[0m\]\[\e[90m\]λ\[\e[0m\] ' 
+
